26425743f2786ad09d59bc0aabb7de9fdd98054d
Rick Walters <rick@walters.xyz>
2025-09-12 15:31:10 -0400

    checkpoint: removed all traces of SciChart

--- gene-management-ui/src/components/SimRealTimeInstrumentation.vue
+++ gene-management-ui/src/components/SimRealTimeInstrumentation.vue
@@ -1,28 +1,13 @@
 <script>
 import {defineComponent} from "vue";
-import {SciChartSurface, NumericAxis} from "scichart";
-
-async function initSciChart() {
-  const {wasmContext, sciChartSurface} = await SciChartSurface.create("scichart-root");
-  sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
-  return sciChartSurface;
-}
 
 export default defineComponent({
   name: "SimRealTimeInstrumentation",
   data() {
-    return {
-      chartInitializationPromise: undefined,
-    };
   },
   mounted() {
-    this.chartInitializationPromise = initSciChart();
   },
   beforeUnmount() {
-    this.chartInitializationPromise.then(sciChartSurface => {
-      sciChartSurface.delete();
-    });
   },
 })
 </script>
@@ -30,7 +15,6 @@ export default defineComponent({
 <template>
   <div class="instrumentation real-time">
     <p>Real-time instrumentation</p>
-    <div id="scichart-root" style="width: 600px; height: 400px;"></div>
   </div>
 </template>
 
--- gene-management-ui/vite.config.js
+++ gene-management-ui/vite.config.js
@@ -1,45 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import fs from 'fs'
-import path from 'path'
-import {viteStaticCopy} from "vite-plugin-static-copy";
-
-// Custom middleware for wasm MIME type
-const wasmMiddleware = () => {
-  return {
-    name: 'wasm-middleware',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url.endsWith('.wasm')) {
-          const wasmPath = path.join(__dirname, 'public', path.basename(req.url));
-          const wasmFile = fs.readFileSync(wasmPath);
-          res.setHeader('Content-Type', 'application/wasm');
-          res.end(wasmFile);
-          return;
-        }
-        next();
-      });
-    },
-  };
-};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasmMiddleware(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/scichart/_wasm/scichart2d.wasm',
-          dest: ''
-        },
-        // {
-        //   src: 'node_modules/scichart/_wasm/scichart2d.data',
-        //   dest: ''
-        // }
-      ]
-    })
-  ],
+  plugins: [vue()],
   css: {
     devSourcemap: true
   },
